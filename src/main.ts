b25e99a2c615cfb669930e5218af37854d2cc91b
Jonathan Noguerales <jonathan.n@pyv.es>
2023-10-20 08:35:14 +0200

    Obtimización main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,48 +1,101 @@
 import "./style.css";
 
-const botonSiguiente = document.querySelector(".siguiente");
-const botonAtras = document.querySelector(".atras");
-const botonReset = document.querySelector(".reset");
-const mostrarNumero = document.querySelector(".numero-turno");
-const numeroInput = document.getElementById("numeroInput") as HTMLInputElement;
-const ingresarButton = document.getElementById("ingresar");
-let contador = 0;
+let contador: number = 0;
 
-ingresarButton?.addEventListener("click", () => {
-    const numero = parseFloat(numeroInput.value); 
-    if(mostrarNumero !== null && mostrarNumero !== undefined){
-        mostrarNumero.textContent = `${numero}`;
-    }
-});
+function reset() {
+  contador = 0;
+  pintarContador();
+}
 
-if(botonReset instanceof HTMLElement){
-    botonReset.addEventListener("click", () => {
-        if(mostrarNumero !== null && mostrarNumero !== undefined){
-            contador = 0;
-            mostrarNumero.textContent = "00";
-            numeroInput.value = " ";
-        }
-})};
+function siguiente() {
+  contador++;
+  pintarContador();
+}
+function atras() {
+  if (contador != 0) {
+    contador--;
+    pintarContador();
+  }
+}
 
-if(botonSiguiente instanceof HTMLElement){
+function ingresar() {
+  const ingresarButton = document.getElementById("numeroInput");
+
+  if (
+    ingresarButton !== null &&
+    ingresarButton !== undefined &&
+    ingresarButton instanceof HTMLInputElement
+  ) {
+    contador = parseInt(ingresarButton.value);
+    pintarContador();
+  } else {
+    console.error("No se encontro el elemento ingresar");
+  }
+}
+
+const pintarContador = () => {
+  const mostrarNumero = document.querySelector(".numero-turno");
+
+  if (
+    mostrarNumero !== null &&
+    mostrarNumero !== undefined &&
+    mostrarNumero instanceof HTMLHeadingElement
+  ) {
+    mostrarNumero.textContent = contador.toString().padStart(2, "0");
+  } else {
+    console.error("No se encontro el elemento numero-turno");
+  }
+};
+
+const eventos = () => {
+  const botonReset = document.querySelector(".reset");
+  if (
+    botonReset !== null &&
+    botonReset !== undefined &&
+    botonReset instanceof HTMLElement
+  ) {
+    botonReset.addEventListener("click", reset);
+  } else {
+    console.error("No se encontro el elemento reset");
+  }
+
+  const botonSiguiente = document.querySelector(".siguiente");
+  if (
+    botonSiguiente !== null &&
+    botonSiguiente !== undefined &&
+    botonSiguiente instanceof HTMLElement
+  ) {
     botonSiguiente.addEventListener("click", siguiente);
-}
-if(botonAtras instanceof HTMLElement){
-    botonAtras.addEventListener("click", atras);
-}
+  } else {
+    console.error("No se encontro el elemento siguiente");
+  }
 
-function siguiente(){
-    contador ++;
-    if(mostrarNumero !== null && mostrarNumero !== undefined){
-        mostrarNumero.textContent = contador.toString().padStart(2, '0');
-    }
-}
-function atras(){
-    if(contador != 0){
-        contador --;
-        if(mostrarNumero !== null && mostrarNumero !== undefined){
-            mostrarNumero.textContent = contador.toString().padStart(2, '0');
-        }
-    }
-}
+  const botonAtras = document.querySelector(".atras");
+  if (
+    botonAtras !== null &&
+    botonAtras !== undefined &&
+    botonAtras instanceof HTMLElement
+  ) {
+    botonAtras.addEventListener("click", atras);
+  } else {
+    console.error("No se encontro el elemento atras");
+  }
+
+  const ingresarButton = document.getElementById("ingresar");
+
+  if (
+    ingresarButton !== null &&
+    ingresarButton !== undefined &&
+    ingresarButton instanceof HTMLElement
+  ) {
+    ingresarButton.addEventListener("click", ingresar);
+  } else {
+    console.error("No se encontro el elemento ingresar");
+  }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  pintarContador();
+  eventos();
+});
 
